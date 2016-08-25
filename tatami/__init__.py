59de5b1d27d4a92e71615ccb107f0279111f425f
Yuichiro Someya <yuichiro-someya@cookpad.com>
2016-08-25 12:51:59 +0900

    Rename tatami to client

--- tatami/__init__.py
+++ tatami/__init__.py
@@ -3,3 +3,10 @@
 __author__ = 'Yuichiro Someya'
 __email__ = 'ayemos.y@gmail.com'
 __version__ = '0.1.0'
+
+from tatami import client
+from tatami import downloader
+from tatami import downloaders
+
+Client = client.Client
+Downloader = downloader.Downloader
